d1fd5075a0a57bbef435364b829606815875b75a
9dongs <144160177+9dongs@users.noreply.github.com>
2026-07-14 10:15:23 +0900

    feat: 일정 추가 요청/응답 DTO 정의 (#2)

---
+++ src/main/java/com/closing/closing/domain/task/dto/TaskResDTO.java
@@ -0,0 +1,47 @@
+package com.closing.closing.domain.task.dto;
+
+import com.closing.closing.domain.task.entity.Task;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+public class TaskResDTO {
+
+    @Builder
+    public record CreateTaskResultDTO(
+            Long taskId,
+            String title,
+
+            @JsonFormat(pattern = "yyyy-MM-dd")
+            LocalDate startDate,
+
+            @JsonFormat(pattern = "yyyy-MM-dd")
+            LocalDate endDate,
+
+            @JsonFormat(pattern = "HH:mm")
+            LocalTime taskTime,
+
+            boolean isCompleted,
+            String source,
+
+            @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssxxx")
+            OffsetDateTime createdAt
+    ) {
+        public static CreateTaskResultDTO from(Task task) {
+            return CreateTaskResultDTO.builder()
+                    .taskId(task.getId())
+                    .title(task.getTitle())
+                    .startDate(task.getStartDate())
+                    .endDate(task.getEndDate())
+                    .taskTime(task.getTaskTime())
+                    .isCompleted(task.isCompleted())
+                    .source(task.getSource().name().toLowerCase())
+                    .createdAt(task.getCreatedAt().atOffset(ZoneOffset.of("+09:00")))
+                    .build();
+        }
+    }
+}
