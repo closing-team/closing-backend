aa2d6818c6e260ee68e53244cf4074302fe5a596
21g <leehan0311@gmail.com>
2026-07-19 12:03:22 +0900

    [Feat] AWS S3 코드 구현
    TODO: AWS 인증 설정 후 S3관련 설정 완료

--- src/main/java/com/closing/closing/global/exception/ErrorCode.java
+++ src/main/java/com/closing/closing/global/exception/ErrorCode.java
@@ -14,7 +14,9 @@ public enum ErrorCode {
     PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "PRODUCT404", "상품을 찾을 수 없습니다."),
     TRADE_LOCATION_REQUIRED(HttpStatus.BAD_REQUEST, "TRADE_LOCATION_REQUIRED", "직거래 장소를 입력해야 합니다."),
     INVALID_IMAGE_COUNT(HttpStatus.BAD_REQUEST, "INVALID_IMAGE_COUNT", "상품 이미지는 1장 이상 10장 이하로 등록해야 합니다."),
-    PRODUCT_DELETE_FORBIDDEN(HttpStatus.FORBIDDEN, "PRODUCT_DELETE_FORBIDDEN", "본인의 상품만 삭제할 수 있습니다.");
+    PRODUCT_DELETE_FORBIDDEN(HttpStatus.FORBIDDEN, "PRODUCT_DELETE_FORBIDDEN", "본인의 상품만 삭제할 수 있습니다."),
+    IMAGE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "IMAGE_UPLOAD_FAILED", "이미지 업로드에 실패했습니다."),
+    IMAGE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "IMAGE_DELETE_FAILED", "이미지 삭제에 실패했습니다.");
     //INVALID_IMAGE_FORMAT(HttpStatus.BAD_REQUEST, "INVALID_IMAGE_FORMAT", "지원하지 않는 이미지 형식입니다.");
 
     private final HttpStatus httpStatus;
