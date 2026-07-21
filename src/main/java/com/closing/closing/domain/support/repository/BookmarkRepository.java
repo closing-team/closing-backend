85573600deccfed4017d95e1fdfec5a1b9075afd
9dongs <144160177+9dongs@users.noreply.github.com>
2026-07-22 02:29:16 +0900

    feat: 지원정보 북마크 저장소 추가 (#41)
    - 사용자와 지원정보 기준 중복 조회 메서드 추가

---
+++ src/main/java/com/closing/closing/domain/support/repository/BookmarkRepository.java
@@ -0,0 +1,9 @@
+package com.closing.closing.domain.support.repository;
+
+import com.closing.closing.domain.support.entity.Bookmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+    boolean existsByUser_IdAndSupportInfo_Id(Long userId, Long supportId);
+}
