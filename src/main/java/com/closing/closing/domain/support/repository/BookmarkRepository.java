74e985211ff21fd92139c5ede6d828747de5e400
9dongs <144160177+9dongs@users.noreply.github.com>
2026-07-22 02:55:12 +0900

    feat: 북마크 삭제 조회 메서드 추가 (#42)

--- src/main/java/com/closing/closing/domain/support/repository/BookmarkRepository.java
+++ src/main/java/com/closing/closing/domain/support/repository/BookmarkRepository.java
@@ -3,7 +3,11 @@ package com.closing.closing.domain.support.repository;
 import com.closing.closing.domain.support.entity.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
     boolean existsByUser_IdAndSupportInfo_Id(Long userId, Long supportId);
+
+    Optional<Bookmark> findByUser_IdAndSupportInfo_Id(Long userId, Long supportId);
 }
